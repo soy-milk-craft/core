b8e85835ecf06114bfeceeb57ff8b8263cb5a4ad
Eunwoo Choi <choi@eunwoo.dev>
2023-06-21 16:56:31 +0900

    feat: initial commit

---
+++ src/main/java/soymilkcraft/soycore/SoyCore.java
@@ -0,0 +1,17 @@
+package soymilkcraft.soycore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SoyCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
